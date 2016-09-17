0a6dfc928d859a8431eb42df21584b192b6e12cc
Lea Christensenmartin <Lea Christensenmartin>
2016-09-17 11:52:25 -0400

    Adds 3 classes that are currenty empty. Adds Top of page documentation to each file

---
+++ assignment1/CSVProcessor.cs
@@ -0,0 +1,16 @@
+﻿/* Jeffrey Martin
+   CIS237 Advanced C3
+   9-20-20196
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment1
+{
+    class CSVProcessor
+    {
+    }
+}
